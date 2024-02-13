31b4b6da6e87246610d4cf78caa7ba0676250672
nekkox <klascibox@gmail.com>
2024-02-13 22:49:07 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+<v-app theme="dark">
+  <v-container class="main">
+    <main>
+    
+      <RouterView />
+    
+    </main>
+  </v-container>
+  <v-footer app>
+    <span>Meal Planner</span>&nbsp;- &copy;
+    {{ new Date().getFullYear() }}
+  </v-footer>
+</v-app>
+</template>
+
+<style scoped>
+
+</style>
