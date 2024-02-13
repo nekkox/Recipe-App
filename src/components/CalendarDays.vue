63947b17f7bb74a93415a32e364caa58b7623889
nekkox <klascibox@gmail.com>
2024-02-13 23:44:53 +0100

    Crete CalendarDays component that generates array of cards, each card has its own date

---
+++ src/components/CalendarDays.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  date: {
+    type: Date,
+    required: true
+  },
+  days: {
+    type: Number,
+    required: true,
+    default: 7
+  }
+})
+
+
+function generateCards(startDate, numberOfDays) {
+  const cards = []
+  const currentDate = new Date(startDate)
+
+  for (let i = 0; i < numberOfDays; i++) {
+    const date = new Date(currentDate.getTime())
+    const content = `Card ${i + 1}`
+    cards.push({ date, content })
+    currentDate.setDate(currentDate.getDate() + 1)
+  }
+  return cards
+}
+
+const cards = ref(generateCards(props.date, props.days))
+console.log(cards);
+</script>
+
+
+<template></template>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
+import CalendarDays from '@/components/CalendarDays.vue'
 
 </script>
 
 <template>
  
    <h1>hello</h1>
+   <CalendarDays :days="7" :date="new Date()">
+
+   </CalendarDays>
 
 </template>
