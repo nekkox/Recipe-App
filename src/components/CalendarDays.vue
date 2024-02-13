e4f2eaab860cac92463b748cc776343202c7e8e2
nekkox <klascibox@gmail.com>
2024-02-13 23:52:40 +0100

    Create table to show the generated cards

--- src/components/CalendarDays.vue
+++ src/components/CalendarDays.vue
@@ -31,5 +31,22 @@ console.log(cards);
 </script>
 
 
-<template></template>
+<template>
+
+<v-table>
+<thead>
+<tr>
+  <th class="text-left">Upcoming days</th>
+</tr>
+</thead>
+<tbody>
+<tr v-for="card in cards" :key="card.date.toString()">
+<td class="py-4">{{ card.content }}</td>
+</tr>
+
+</tbody>
+
+</v-table>
+
+</template>
 
